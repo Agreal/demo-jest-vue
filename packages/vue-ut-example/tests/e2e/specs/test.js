329eb18c65ec363eed782ee9a665988783618691
Qin Xiaoqiang <qxq111@gmail.com>
2019-05-08 14:17:53 +0800

    test: add vue e2e test

--- packages/vue-ut-example/tests/e2e/specs/test.js
+++ packages/vue-ut-example/tests/e2e/specs/test.js
@@ -1,8 +1,25 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
+/**
+ * 选择元素
+ * https://docs.cypress.io/guides/core-concepts/introduction-to-cypress.html#Querying-Elements
+ *
+ * 断言方法
+ * https://docs.cypress.io/guides/references/assertions.html#Common-Assertions
+ */
+describe('Home Page', () => {
   it('Visits the app root url', () => {
     cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
+    cy.contains('h1', 'Latest Vue.js Commits')
+
+    cy.get('#master').should('be.checked')
+    cy.get('p').contains('vuejs/vue@master')
+    cy.get('ul > li').should('have.length', 3)
+
+    // change dev
+    cy.get('#dev').check()
+    cy.get('#master').should('not.be.checked')
+    cy.get('p').contains('vuejs/vue@dev')
+    cy.get('ul > li').should('have.length', 3)
   })
 })
